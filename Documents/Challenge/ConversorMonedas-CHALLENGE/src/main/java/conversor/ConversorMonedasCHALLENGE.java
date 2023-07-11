a3647fac8006979b07b9fecccb180bf108f484c0
Angello Vasconez <alvascon@espol.edu.ec>
2023-07-10 20:36:19 -0500

    inicio

---
+++ Documents/Challenge/ConversorMonedas-CHALLENGE/src/main/java/conversor/ConversorMonedasCHALLENGE.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package conversor;
+
+/**
+ *
+ * @author vv
+ */
+public class ConversorMonedasCHALLENGE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
